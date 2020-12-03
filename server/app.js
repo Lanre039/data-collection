913ef749b97a3e71e0c4d5c895b34b7cd1d9b444
Lanre039 <rajilat1000@gmail.com>
2020-12-03 07:12:25 +0100

    added concurrent

--- server/app.js
+++ server/app.js
@@ -31,12 +31,6 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
